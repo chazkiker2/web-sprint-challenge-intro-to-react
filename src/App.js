623dc1ca6bf396d8601b40af9c99a3ea3dea6f2a
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-10-09 12:02:39 -0400

    get request functions, Characters render

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import axios from "axios";
+import { BASE_URL, ENDPOINTS, getQueryString } from "./constants/index";
+
+import Character from "./components/Character";
+// console.log(BASE_URL, ENDPOINTS);
+
+// console.log(axios);
+
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+	/**
+	 * Try to think through what state you'll need for this app before starting. Then build out
+	 * the state properties here.
+	 */
+	const [data, setData] = useState({});
+	const [characters, setCharacters] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+	/**
+	 * Fetch characters from the API in an effect hook. Remember, anytime you have a
+	 * side effect in a component, you want to think about which state and/or props it should
+	 * sync up with, if any.
+	 */
+	useEffect(() => {
+		const queryString = getQueryString("characters", null, null);
+		const fetchCount = () => {
+			axios.get(queryString)
+				.then(res => {
+					console.log(res.data);
+					console.log(res.data.results);
+					setData(res.data);
+					setCharacters(res.data.results);
+				})
+				.catch(err => {
+					console.log(err);
+					debugger;
+				});
+		};
+		if (queryString) {
+			console.log(queryString);
+			fetchCount();
+		}
+	}, []);
+
+	return (
+		<div className="App">
+			<h1 className="Header">Characters</h1>
+			<ul>
+			{
+				characters.map(char => {
+					return <li key={char.id}><Character character={char} /></li> 
+				})
+			}
+
+			</ul>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
-// Write your Character component here
+import React from "react";
+// import axios from "axios";
+
+const Character = (props) => {
+	const { character } = props;
+	const { created, episode, gender, id, image, location, name, origin, species, status, type, url } = character;
+
+	return (
+		<>
+			<h1>{name}</h1>
+
+			<img src={image} />
+		</>
+	);
+};
+
+export default Character;
